6598a1812f0bd2278af3b51b00ff73507c4164bc
Harsh Panghal <102521089+harsh12panghal@users.noreply.github.com>
2024-06-11 16:44:47 +0530

    Add files via upload

---
+++ court_line_detector/__init__.py
@@ -0,0 +1,1 @@
+from .court_lint_detector import CourtLineDetector
---
+++ yolo_inference.py
@@ -0,0 +1,10 @@
+from ultralytics import YOLO
+
+model = YOLO('models/yolo5_best.pt')
+
+result = model.predict('input_videos/input_video.mp4',conf = 0.2, save=True) 
+
+print(result)
+print("boxes:")
+for box in result[0].boxes:
+    print(box)
